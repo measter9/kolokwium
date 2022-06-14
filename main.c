1906ce0c53a40d26e37d464e31be7005f1dd06c5
measter9 <79282125+measter9@users.noreply.github.com>
2022-06-14 18:11:50 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
-	printf("hello world!");
+	pid_t pid1,pid2,pid3,pid4;
+
+	pid1=fork();
+	printf("%d \n",pid1);
+	pid2=fork();
+	printf("%d \n",pid2);
+	pid3=fork();
+	printf("%d \n",pid3);
+	pid4=fork();
+	printf("%d \n",pid4);	
+
+
+
 	return 0;
 }
